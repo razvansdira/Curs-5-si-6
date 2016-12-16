485424c22eb410f7b152bcfb56c814f5efc4cb6e
razvan.sdira <sdira_razvan@yahoo.com>
2016-12-16 13:41:13 +0200

    Curs 5 si 6

---
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class ProjectController:Controller
+    {
+        public ActionResult DoAnAction()
+        {
+            return Content("Hello world!");
+        }
+
+        public ActionResult HelloUser(string alpha)
+        {
+            var encodedName = Server.HtmlEncode(alpha);
+            //return Content("Hello "+ alpha);
+            return Content("Hellooo " ,encodedName);
+        }
+    }
+}
